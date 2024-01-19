48e5f74d737a88e6fded77e69a78e15823ca253b
Restyled.io <commits@restyled.io>
2024-01-19 13:56:54 +0000

    Restyled by clang-format

--- xap-core/xap-datagrid/src/main/java/com/sun/jini/reggie/ServiceTypeBase.java
+++ xap-core/xap-datagrid/src/main/java/com/sun/jini/reggie/ServiceTypeBase.java
@@ -1,19 +1,19 @@
 /*
- * 
+ *
  * Copyright 2005 Sun Microsystems, Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * 	http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 package com.sun.jini.reggie;
 
@@ -28,55 +28,57 @@ import java.rmi.server.RMIClassLoader;
 @com.gigaspaces.api.InternalApi
 public class ServiceTypeBase implements Serializable {
 
-    private static final boolean support_code_base = Boolean.getBoolean("com.gs.transport_protocol.lrmi.support-codebase");
+  private static final boolean support_code_base =
+      Boolean.getBoolean("com.gs.transport_protocol.lrmi.support-codebase");
 
-    private static final long serialVersionUID = 2L;
+  private static final long serialVersionUID = 2L;
 
-    /**
-     * The ServiceType.
-     *
-     * @serial
-     */
-    public final ServiceType type;
-    /**
-     * The codebase.
-     *
-     * @serial
-     */
-    public String codebase;
+  /**
+   * The ServiceType.
+   *
+   * @serial
+   */
+  public final ServiceType type;
+  /**
+   * The codebase.
+   *
+   * @serial
+   */
+  public String codebase;
 
-    /**
-     * Simple constructor
-     */
-    public ServiceTypeBase(ServiceType type, String codebase) {
-        this.type = type;
-        this.codebase = codebase;
-    }
+  /**
+   * Simple constructor
+   */
+  public ServiceTypeBase(ServiceType type, String codebase) {
+    this.type = type;
+    this.codebase = codebase;
+  }
 
-    /**
-     * Sets the codebase to the codebase of the given class.
-     */
-    public void setCodebase(Class cls) {
-        codebase = support_code_base ? RMIClassLoader.getClassAnnotation(cls) : null;
-    }
+  /**
+   * Sets the codebase to the codebase of the given class.
+   */
+  public void setCodebase(Class cls) {
+    codebase =
+        support_code_base ? RMIClassLoader.getClassAnnotation(cls) : null;
+  }
 
-    /**
-     * Converts an array of ServiceTypeBase to an array of Class.  If a class cannot be loaded, it
-     * is left as null.
-     */
-    public static Class[] toClass(ServiceTypeBase[] stypes) {
-        Class[] classes = null;
-        if (stypes != null) {
-            classes = new Class[stypes.length];
-            for (int i = stypes.length; --i >= 0; ) {
-                try {
-                    ServiceTypeBase stype = stypes[i];
-                    classes[i] = stype.type.toClass(stype.codebase);
-                } catch (Throwable e) {
-                    RegistrarProxy.handleException(e);
-                }
-            }
+  /**
+   * Converts an array of ServiceTypeBase to an array of Class.  If a class
+   * cannot be loaded, it is left as null.
+   */
+  public static Class[] toClass(ServiceTypeBase[] stypes) {
+    Class[] classes = null;
+    if (stypes != null) {
+      classes = new Class[stypes.length];
+      for (int i = stypes.length; --i >= 0;) {
+        try {
+          ServiceTypeBase stype = stypes[i];
+          classes[i] = stype.type.toClass(stype.codebase);
+        } catch (Throwable e) {
+          RegistrarProxy.handleException(e);
         }
-        return classes;
+      }
     }
+    return classes;
+  }
 }
